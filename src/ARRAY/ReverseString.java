a58896de1c617b704aa53ecb5188a5e4842d78e2
dhruv264965 <dhruv264965shukla@gmail.com>
2026-06-27 22:04:40 +0530

    commit

--- src/ARRAY/ReverseString.java
+++ src/ARRAY/ReverseString.java
@@ -1,16 +1,56 @@
-package ARRAY;
-
-public class ReverseString {
-    public static void main(String[] args) {
-
-
-        String originalStr = "Hello";
-        String reversedStr = "";
-
-        for (int i = 0; i < originalStr.length(); i++) {
-            reversedStr = originalStr.charAt(i) + reversedStr;
-        }
-
-        System.out.println("Reversed string: " + reversedStr);
-    }
-}
+//package ARRAY;
+//
+//import java.util.Arrays;
+//
+//public class ReverseString {
+//    public static void main(String[] args) {
+//
+//        public static List<Integer> countRotations ( int nCogwheels, List<List<Integer>>connections){
+//            // 1. Build Adjacency List
+//            List<List<Integer>> adj = new ArrayList<>();
+//            for (int i = 0; i < nCogwheels; i++) {
+//                adj.add(new ArrayList<>());
+//            }
+//            for (List<Integer> conn : connections) {
+//                int u = conn.get(0);
+//                int v = conn.get(1);
+//                adj.get(u).add(v);
+//                adj.get(v).add(u);
+//            }
+//
+//            // 2. Array to store directions: 0 = unvisited, 1 = Clockwise, -1 = Counter-Clockwise
+//            int[] directions = new int[nCogwheels];
+//            Queue<Integer> queue = new LinkedList<>();
+//
+//            // Start with gear 0 rotating Clockwise (1)
+//            directions[0] = 1;
+//            queue.add(0);
+//
+//            while (!queue.isEmpty()) {
+//                int current = queue.poll();
+//
+//                for (int neighbor : adj.get(current)) {
+//                    if (directions[neighbor] == 0) {
+//                        // Assign opposite direction
+//                        directions[neighbor] = -directions[current];
+//                        queue.add(neighbor);
+//                    } else if (directions[neighbor] == directions[current]) {
+//                        // Deadlock detected: two connected gears trying to move the same way
+//                        return Arrays.asList(-1, -1);
+//                    }
+//                }
+//            }
+//
+//            // 3. Count results
+//            int clockwiseCount = 0;
+//            int counterClockwiseCount = 0;
+//            for (int dir : directions) {
+//                if (dir == 1) clockwiseCount++;
+//                else if (dir == -1) counterClockwiseCount++;
+//            }
+//
+//            return Arrays.asList(clockwiseCount, counterClockwiseCount);
+//        }
+//    }
+//}
+//}
--- src/Collection/HashMap/HashMapDemo.java
+++ src/Collection/HashMap/HashMapDemo.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 
 public class HashMapDemo {
     public static void main(String[] args) {
+
         HashMap<String,String>hp=new HashMap<>();
         hp.put("dhruv","shukla");
         System.out.println(hp);
         hp.put(new String("dhruv"),"mishra");
         System.out.println(hp.size());
         System.out.println(hp);
+
     }
 }
